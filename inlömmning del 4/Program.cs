7fd1331954f1cdd0eb2110ece136cc9f3cbdcf03
absa07002 <absa07002@DESKTOP-TAGHD5A>
2024-10-06 20:38:02 +0200

    Add project files.

---
+++ inlömmning del 4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Uppgift_2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("lön för arbetare 1");
+            int arb1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("lön för arbetare 2");
+            int arb2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("lön för arbetare 3");
+            int arb3 = int.Parse(Console.ReadLine());
+            int sum = (arb1 + arb2 + arb3) / 3;
+            Console.WriteLine(sum);
+        }
+    }
+}
